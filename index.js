9196261b839c8fb2d3f899aa6efbf549032a4289
moses <moses.kamau@student.moringaschool.com>
2023-07-07 20:40:30 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution here
+function findMatching(){
+    const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+    const result = drivers.filter( driver => driver ==driver)
+
+}
